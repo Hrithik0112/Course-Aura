711c082130a7606636546395924efd76af81774a
Hrithik <slumdog4321@gmail.com>
2024-09-01 11:05:45 +0530

    Feat : Clerk Authentication added

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const DashBoard = () => {
+  return (
+    <div>
+        <UserButton/>
+    </div>
+  )
+}
+
+export default DashBoard
